74e828a65ffe71dec10daf1615b90303ab57b750
Никита <NVRudy@gmail.com>
2023-08-26 21:56:39 +0300

    hw12

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,40 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import homework12.Author;
+import homework12.Book;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    public static void createSpace() {
+        System.out.println("~~~~~~~~~~~~~~~");
     }
+
+    public static void main(String[] args) {
+        Author author = new Author("Николай ", "Гоголь");
+        Book book = new Book("\"Мертвые души\"", author.getName(), 1842);
+        createSpace();
+        System.out.println("Название книги: " + book.getBookName());
+        System.out.println("Автор:  " + author.getName() + author.getSurName());
+        System.out.println("Год издания: " + book.getYearOfPublishing());
+        book.setYearOfPublishing(1845);
+        System.out.println("Год издания (новый): " + book.getYearOfPublishing());
+        createSpace();
+        Author author1 = new Author("Александр ", "Дюма");
+        Book book1 = new Book("\"Граф Монте-Кристо\"", author1.getName(), 1844);
+        System.out.println("Название книги: " + book1.getBookName());
+        System.out.println("Автор: " + author1.getName() + author1.getSurName());
+        System.out.println("Год издания: " + book1.getYearOfPublishing());
+        book1.setYearOfPublishing(1861);
+        System.out.println("Год издания (новый): " + book1.getYearOfPublishing());
+        createSpace();
+        Author author2 = new Author("Эрих Мария ", "Ремарк");
+        Book book2 = new Book("\"Три товарища\"", author2.getName(), 1936);
+        System.out.println("Название книги: " + book2.getBookName());
+        System.out.println("Автор: " + author2.getName() + author2.getSurName());
+        System.out.println("Год издания: " + book2.getYearOfPublishing());
+        book2.setYearOfPublishing(1984);
+        System.out.println("Год издания (новый): " + book2.getYearOfPublishing());
+        createSpace();
+
+    }
+
+
 }
---
+++ src/homework12/Book.java
@@ -0,0 +1,29 @@
+package homework12;
+
+public class Book {
+    private String bookName;
+    private String author;
+    private int yearOfPublishing;
+
+    public Book(String bookName, String author, int yearOfPublishing) {
+        this.bookName = bookName;
+        this.author = author;
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getYearOfPublishing() {
+        return this.yearOfPublishing;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+}
